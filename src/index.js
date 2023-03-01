c3dd84378c6f187033d2a2b3c793e39ef1569d64
WonderAlice <cuddlealice@gmail.com>
2023-03-01 22:02:12 +0100

    Updated UI for login screen

--- src/index.js
+++ src/index.js
@@ -1,5 +1,10 @@
-const { app, BrowserWindow } = require('electron');
+const { app, BrowserWindow, ipcMain, nativeTheme } = require('electron');
 const path = require('path');
+const url = require('url');
+const net = require('net');
+const electron = require("electron");
+
+var socketClient
 
 // Handle creating/removing shortcuts on Windows when installing/uninstalling.
 if (require('electron-squirrel-startup')) {
@@ -11,6 +16,8 @@ const createWindow = () => {
   const mainWindow = new BrowserWindow({
     width: 800,
     height: 600,
+    titleBarStyle: 'hidden',
+    icon: path.join(__dirname, 'src/Images/icon.png'),
     webPreferences: {
       preload: path.join(__dirname, 'preload.js'),
     },
@@ -19,8 +26,50 @@ const createWindow = () => {
   // and load the index.html of the app.
   mainWindow.loadFile(path.join(__dirname, 'index.html'));
 
+  const app = electron.app;
+  const icon = electron.nativeImage.createFromPath(app.getAppPath() + "/src/Images/icon.png");
+  console.log(icon.getSize())
+  app.dock.setIcon(icon);
+
+  //toggle that detects if dark mode is enabled in the system, changing the scheme colors
+  ipcMain.handle('dark-mode:toggle', () => {
+    if (nativeTheme.shouldUseDarkColors) {
+      nativeTheme.themeSource = 'light'
+    } else {
+      nativeTheme.themeSource = 'dark'
+    }
+    return nativeTheme.shouldUseDarkColors
+  })
+
+  ipcMain.handle('dark-mode:system', () => {
+    nativeTheme.themeSource = 'system'
+  })
+
+
+  //console.log('Try to connect');
+  //socketClient = net.connect({host:'localhost', port:5001},  () => {
+    // 'connect' listener
+    //console.log('connected to server!');
+    //socketClient.write('world!\r\n');
+  //});
+
+  //socketClient.on('data', (data) => {
+    //console.log(data.toString());
+    //var person = JSON.parse(data);
+
+    //console.log('Hello !');
+
+  //});
+  //socketClient.on('end', () => {
+  //  console.log('disconnected from server');
+  //});
+
+  app.on('before-quit',function(){
+    //socketClient.end();
+  })
+
   // Open the DevTools.
-  mainWindow.webContents.openDevTools();
+//mainWindow.webContents.openDevTools();
 };
 
 // This method will be called when Electron has finished
@@ -32,9 +81,9 @@ app.on('ready', createWindow);
 // for applications and their menu bar to stay active until the user quits
 // explicitly with Cmd + Q.
 app.on('window-all-closed', () => {
-  if (process.platform !== 'darwin') {
+  //if (process.platform !== 'darwin') {
     app.quit();
-  }
+ // }
 });
 
 app.on('activate', () => {
