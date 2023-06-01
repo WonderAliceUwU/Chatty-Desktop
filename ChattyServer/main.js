df4f6af69cb87bb29236a7ad8ca5cc6c45bc0e6a
WonderAlice <cuddlealice@gmail.com>
2023-06-01 13:48:41 +0200

    Edited default server port

--- ChattyServer/main.js
+++ ChattyServer/main.js
@@ -554,8 +554,8 @@ app.post('/api/images', upload.single('image'), (req, res) => {
 });
 
 // Start the server and listen for WebSocket connections
-server.listen(3000, '0.0.0.0', () => {
-    console.log(`Server started on port ${3000}`);
+server.listen(443, '0.0.0.0', () => {
+    console.log(`Server started on port ${443}`);
 });
 
 // Handle WebSocket connections
